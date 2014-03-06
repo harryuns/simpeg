1d3a903c48acc385e8f610bae50a67125cb2f2f0
her0satr <her0satr@yahoo.com>
2014-03-05 19:32:26 -0800

    update

---
+++ Session.php
@@ -0,0 +1,16 @@
+<?php
+	session_start();
+	print_r($_SESSION);
+	print_r($_COOKIE);
+	
+	$_SESSION['UserLogin'] = array(
+		'UserID' => 'adminsimpeg',
+		'UserName' => 'adminsimpeg',
+		'UserDisplay' => 'adminsimpeg',
+		'UserGroupID' => '1013',
+		'TimeActive' => time(),
+		'ApplicationRequest' => 'Simpeg',
+		'LinkIndex' => 'http://devel184.ub.ac.id/simpeg/index.php/Pegawai',
+		'Fakultas' => array( 'Title' => 'Seluruh fakultas', 'ID' => 'x' )
+	);
+?>
